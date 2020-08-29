0cb66d4a7613713740dfecee1705cc1be8a5cdeb
yuki-tea <68893628+yuki-tea@users.noreply.github.com>
2020-08-29 21:52:02 +0900

    messagekinou

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,18 +15,18 @@ class GroupsController < ApplicationController
     else
       render :new
     end
+  end
 
-    def edit
-      @group = Group.find(params[:id])
-    end
+  def edit
+    @group = Group.find(params[:id])
+  end
 
-    def update
-      @group = Group.find(params[:id])
-      if @group.update(group_params)
-        redirect_to root_path, notice: 'グループを更新しました'
-      else
-        render :edit
-      end
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
     end
   end
 
