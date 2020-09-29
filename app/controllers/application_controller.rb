234abce6ea25713c62c88c5356d7b26e1aa8f04c
yuki-tea <68893628+yuki-tea@users.noreply.github.com>
2020-09-29 16:37:23 +0900

    basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
+
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 
   protected
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,4 @@
 server '54.168.220.178', user: 'ec2-user', roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
